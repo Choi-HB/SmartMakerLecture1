59a3228c74794042669d146454564dd4bab33a77
Jaehong, Oh <jhoh@idealink.kr>
2015-06-09 15:24:38 +0900

    프로젝트 추가 완료

---
+++ Assets/MyLecture/Scripts/MyData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyData : MonoBehaviour
+{
+	public string name;
+	public Texture image;
+	public int age;
+	public string phoneNumber;
+}
